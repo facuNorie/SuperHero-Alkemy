0e868b1c6194a5202e0b0418bfc3cdab22223d46
facuNorie <noriegafacundo08@gmail.com>
2021-09-24 21:08:33 -0300

    Cambiando contextApi por redux

--- src/components/ResultsOfSearch/index.jsx
+++ src/components/ResultsOfSearch/index.jsx
@@ -2,32 +2,34 @@ import React, { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
 import Spinner from ".././Spinner";
 import CardHero from "../CardHero";
+import { useSelector } from "react-redux";
 import "./style.css";
 export default function ResulsOfSearch() {
-	const { listOfResults, top, spinner, myTeam } = useContext(AppContext);
-	const idAllTeam = myTeam.map(hero => hero.id);
-
-	return (
-		<div
-			className={
-				top
-					? "h-75 w-100 container-fluid overflow-auto rounded containerResults"
-					: "false"
-			}
-			style={top ? { backdropFilter: "blur(5px)" } : { overflow: "auto" }}
-		>
-			{spinner ? <Spinner /> : <span></span>}
-			<div className="row">
-				{listOfResults?.map(hero => {
-					return (
-						<CardHero
-							key={hero.id}
-							hero={hero}
-							is_in_my_team={idAllTeam.includes(hero.id) ? true : false}
-						/>
-					);
-				})}
-			</div>
-		</div>
-	);
+  const listOfResults = useSelector((state) => state?.searchResults);
+  const myTeam = useSelector((state) => state?.myTeam);
+  const { top, spinner } = useContext(AppContext);
+  const idAllTeam = myTeam?.map((hero) => hero.id);
+  return (
+    <div
+      className={
+        top
+          ? "h-75 w-100 container-fluid overflow-auto rounded containerResults"
+          : "false"
+      }
+      style={top ? { backdropFilter: "blur(5px)" } : { overflow: "auto" }}
+    >
+      {spinner ? <Spinner /> : <span></span>}
+      <div className="row">
+        {listOfResults?.map((hero) => {
+          return (
+            <CardHero
+              key={hero.id}
+              hero={hero}
+              is_in_my_team={idAllTeam.includes(hero.id) ? true : false}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -2,62 +2,78 @@ import React, { useContext } from "react";
 import { Formik, Form, Field } from "formik";
 import { AppContext } from "../../context/AppContext";
 import ErrorInput from "../ErrorInput";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { getHerosResults } from "../../actions/herosActions";
 
 export default function SearchForm() {
-	const { getSuperHero, top, setTop, setListOfResults, setSpinner } =
-		useContext(AppContext);
-	const searchSuperHero = search => {
-		setListOfResults([]);
-		getSuperHero(search);
-		setTop(true);
-		setSpinner(true);
-	};
-	return (
-		<div className="w-50">
-			{!top && (
-				<h2 className="text-light shadow-sm bg-dark py-4 rounded text-center">
-					Find heroes for your team
-				</h2>
-			)}
-			<Formik
-				initialValues={{ search: "" }}
-				validate={values => {
-					const errors = {};
-					if (!values.search) {
-						errors.search = "⚠️ Empty field";
-					}
-					return errors;
-				}}
-				onSubmit={(values, { setSubmitting }) => {
-					setTimeout(() => {
-						searchSuperHero(values.search);
-						setSubmitting(false);
-					}, 400);
-				}}
-			>
-				{({ isSubmitting }) => (
-					<Form>
-						<div className="input-group mb-1">
-							<Field
-								type="text"
-								name="search"
-								className="form-control "
-								placeholder="Search superhero"
-								aria-label="Search superhero"
-								aria-describedby="basic-addon2"
-								autoComplete="off"
-							/>
+  const dispatch = useDispatch();
+  const { top, setTop, setListOfResults, setSpinner } = useContext(AppContext);
+  const getSuperHero = (hero) => {
+    try {
+      axios
+        .get(`https://superheroapi.com/api.php/372344864528011/search/${hero}`)
+        .then((res) => {
+          dispatch(getHerosResults(res?.data.results));
+          setSpinner(false);
+        })
+        .catch((e) => console.log(e));
+    } catch (error) {
+      console.log(error);
+    }
+    return;
+  };
+  const searchSuperHero = (search) => {
+    getSuperHero(search);
+    setTop(true);
+    setSpinner(true);
+  };
+  return (
+    <div className="w-50">
+      {!top && (
+        <h2 className="text-light shadow-sm bg-dark py-4 rounded text-center">
+          Find heroes for your team
+        </h2>
+      )}
+      <Formik
+        initialValues={{ search: "" }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.search) {
+            errors.search = "⚠️ Empty field";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            searchSuperHero(values.search);
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div className="input-group mb-1">
+              <Field
+                type="text"
+                name="search"
+                className="form-control "
+                placeholder="Search superhero"
+                aria-label="Search superhero"
+                aria-describedby="basic-addon2"
+                autoComplete="off"
+              />
 
-							<div className="input-group-append">
-								<button className="btn btn-primary" type="submit">
-									Search
-								</button>
-							</div>
-						</div>
-						<ErrorInput name="search" />
-					</Form>
-				)}
-			</Formik>
-		</div>
-	);
+              <div className="input-group-append">
+                <button className="btn btn-primary" type="submit">
+                  Search
+                </button>
+              </div>
+            </div>
+            <ErrorInput name="search" />
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
