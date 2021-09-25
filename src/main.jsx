054eae46a75df5649fa23859d391c2cae7276b5b
facuNorie <noriegafacundo08@gmail.com>
2021-09-24 21:05:47 -0300

    Creando la store

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import herosReducer from "./reducers/herosReducer";
+const store = createStore(
+  herosReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
