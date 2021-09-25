377b5c0383593b66dc2138c41d37dce85ad49f51
facuNorie <noriegafacundo08@gmail.com>
2021-09-25 00:08:55 -0300

    Cambiando contetxApi por redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { addHeroToMyTeam } from "./actions/herosActions";
 import Routes from "./routes";
-import { AppProvider } from "./context/AppContext";
-
+import { useDispatch } from "react-redux";
 function App() {
-	return (
-		<div
-			style={{
-				backgroundImage: `url(https://wallpapercave.com/wp/wp8793838.jpg)`,
-				height: "100vh",
-				overflow: "hidden",
-			}}
-		>
-			<AppProvider>
-				<Routes />
-			</AppProvider>
-		</div>
-	);
+  const dispatch = useDispatch();
+  const getHeroesLocalStorage = () => {
+    const heroes = JSON.parse(localStorage.getItem("myTeam"));
+    heroes.map((hero) => {
+      dispatch(addHeroToMyTeam(hero, hero.biography.alignment));
+    });
+  };
+  useEffect(() => {
+    JSON.parse(localStorage.getItem("myTeam")) !== null
+      ? getHeroesLocalStorage()
+      : console.log("algo salio mal");
+  }, []);
+  return (
+    <div
+      style={{
+        backgroundImage: `url(https://wallpapercave.com/wp/wp8793838.jpg)`,
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
